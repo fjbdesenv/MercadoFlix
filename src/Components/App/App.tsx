f83caf1c77792b59fa3b3f1f232cb01ce1b3eecc
= <=>
2022-09-05 18:56:05 -0300

    Adição de Header

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Footer from '../Footer/Footer';
+import {Header, Footer} from '../components';
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Footer />
     </div>
   );
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Header.css";
+
+function Header (){
+    return (
+        <header>
+            header
+        </header>
+    );
+}
+
+export default Header;

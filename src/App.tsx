df71d013ff5a854c39f4638839e83e8546ee3018
= <=>
2022-09-05 18:04:26 -0300

    Adição de rodape

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Desenvolvido por Fábio Júnior Barbosa
+            </p>
+            <p>
+                <a href="mailto:fjb.desenv@gmail.com">fjb.desenv@gmail.com</a>
+            </p>     
+        </footer>
+    );
+}
+
+export default Footer;

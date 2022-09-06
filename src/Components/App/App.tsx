3824347b64f7cc9ffb30a4764e401ae9c5fcd8cc
= <=>
2022-09-06 08:51:30 -0300

    Adicionando redirecionado para home

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -12,6 +12,7 @@ function App() {
           <Header />
           
           <Routes>
+            <Route path=''  element={<Home/>}/>
             <Route path='home' element={<Home/>}/>
             <Route path='agenda' element={<TimeLine/>}/>
             <Route path='cotacoes' element={<Prices/>}/>

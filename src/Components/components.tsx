6c375ab374c6cf5da5ac50b1794de34420bee129
= <=>
2022-09-06 06:27:01 -0300

    Adição de App em index(components)

--- src/Components/components.tsx
+++ src/Components/components.tsx
@@ -1,5 +1,6 @@
+import App from "./App/App";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import MenuItem from "./Menu/Menu-Item/MenuItem";
 
-export {Footer, Header, MenuItem};
+export {App, Footer, Header, MenuItem};
